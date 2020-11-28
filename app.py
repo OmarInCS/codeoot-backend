09fa3417ec8914d7bcf4ae58366bf0b00677a830
OmarInCS <omarincs@gmail.com>
2020-11-28 22:44:34 +0300

    update CORS

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PUSH')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
     response.headers.add('Access-Control-Allow-Origin', 'https://codeoot-frontend.herokuapp.com')
     return response
 
